a7759d8b82667f5861c78794259b67feb7393d41
Sheela G. Khusal <sheela.khusal.08@gmail.com>
2022-03-21 04:38:00 +0100

    Add some tests

--- tests/test.sh
+++ tests/test.sh
@@ -6,7 +6,7 @@
 #    By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/16 22:02:02 by sguilher          #+#    #+#              #
-#    Updated: 2022/03/16 22:02:04 by sguilher         ###   ########.fr        #
+#    Updated: 2022/03/20 19:19:27 by sguilher         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,10 +18,29 @@
 # error messages 
 echho 123 # command not found
 
-# arquivo de entrada não existe
+## ------------------ arquivo de entrada não existe ------------------------##
 < infile ls -l | wc -l > outfile # errno 2, não roda o primeiro comando, mas roda o segundo
 grep
 grep p
-< error.txt > outfile #(arquivo de input existe, cria o arquivo de saída)
-< infile catzzz | wc -w > outfile #não roda o primeiro comando (dá erro), mas roda o segundo
-< infile catzzz | bonjour | wc -l > outfile #não roda o primeiro e o segundo comando (dá erro), mas roda o segundo
+
+< error.txt > outfile
+#(arquivo de input existe, cria o arquivo de saída)
+
+< infile catzzz | wc -w > outfile
+#não roda o primeiro comando (dá erro), mas roda o segundo
+
+< infile catzzz | bonjour | wc -l > outfile
+#não roda o primeiro e o segundo comando (dá erro), mas roda o segundo
+
+< infile lss | wcc -l > outfile
+## ambos comandos não existem e arquivo de entrada não existe:
+# msg de erro "bash: infile: No such file or directory"
+# msg de erro "bash: wcc: command not found" apenas para o último comando
+# cria o arquivo de outfile ou deixa ele vazio se ele existe
+
+## ------------------ arquivo de entrada existe ------------------------##
+## ambos comandos não existem
+< infile lss | wcc -l > outfile
+# cria o arquivo de outfile ou deixa ele vazio se ele existe
+# msg de erro "bash: lss: command not found"
+# msg de erro "bash: wcc: command not found"
