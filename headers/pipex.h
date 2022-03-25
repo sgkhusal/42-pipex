7fc89574f55c9d350a0ef42330b0f5438c0c753d
Sheela G. Khusal <sheela.khusal.08@gmail.com>
2022-03-25 22:28:53 +0100

    Add new functions and update file name

--- headers/pipex.h
+++ headers/pipex.h
@@ -6,7 +6,7 @@
 /*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 20:54:13 by coder             #+#    #+#             */
-/*   Updated: 2022/03/25 05:03:37 by sguilher         ###   ########.fr       */
+/*   Updated: 2022/03/25 22:15:49 by sguilher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,17 +40,27 @@ typedef struct s_pipex
 	char	**exec_paths;
 }				t_pipex;
 
+typedef struct s_cmd_split
+{
+	char	*tmp;
+	char	**aux;
+	int		i;
+	int		quote_pos;
+}				t_cmd_split;
+
 void	pipex_init(t_pipex *data, int argc, char *argv[], char *envp[]);
 void	pipex_child(t_pipex *data, int i, char *envp[]);
 void	pipex_close(t_pipex *data);
 void	pipex_error(t_pipex *data, char *msg);
+void	pipex_error2(t_pipex *data, char *msg);
 void	pipex_cmd_args_split(t_pipex *data, t_cmd *cmd, char *args);
+void	pipex_quote_error(t_pipex *data, char **aux);
 
 char	*ft_strsjoin(int n, char *str1, ...);
 void	ft_strjoin_free(char **str1, char *str2);
 char	**ft_strarrayjoin(char **array, char *str);
 void	ft_arraysjoin(char **array1, char **array2);
 int		ft_char_pos(char *str, char c);
-void	clean_ptrptr(void **ptr);
+void	ft_clean_ptrptr(void **ptr);
 
 #endif
