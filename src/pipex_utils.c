07f4edc2c2a64207398802c8ad0acaebf32f5bd1
Sheela G. Khusal <sheela.khusal.08@gmail.com>
2022-03-25 05:34:49 +0100

    Add ft_char_pos, ft_strarrayjoin and ft_arraysjoin functions

--- src/pipex_utils.c
+++ src/pipex_utils.c
@@ -6,7 +6,7 @@
 /*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 01:31:35 by sguilher          #+#    #+#             */
-/*   Updated: 2022/03/24 04:19:43 by sguilher         ###   ########.fr       */
+/*   Updated: 2022/03/25 05:30:31 by sguilher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,4 +44,60 @@ char	*ft_strsjoin(int n, char *str1, ...)
 	}
 	va_end(strs);
 	return(aux);
+}
+
+int	ft_char_pos(char *str, char c)
+{
+	int	i;
+
+	i = 0;
+	while(str[i])
+	{
+		if (str[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+char	**ft_strarrayjoin(char **array, char *str)
+{
+	int		size;
+	int		i;
+	char	**new_array;
+
+	if (!array && !str)
+		return (NULL);
+	else if (!str)
+		return (array);
+	size = 0;
+	if (array != NULL)
+	{
+		while (array[size] != NULL)
+			size++;
+	}
+	new_array = (char **)malloc((size + 2) * sizeof(char *));
+	if (!new_array)
+		return (NULL);
+	i = -1;
+	while (++i < size)
+		new_array[i] = ft_strdup(array[i]);
+	new_array[i] = ft_strdup(str);
+	new_array[i + 1] = NULL;
+	return (new_array);
+}
+
+void	ft_arraysjoin(char **array1, char **array2)
+{
+	int		i;
+	char	**aux;
+	
+	i = 0;
+	while (array2[i])
+	{
+		aux = array1;
+		array1 = ft_strarrayjoin(aux, array2[i]);
+		clean_ptrptr((void **)aux);
+		i++;
+	}
 }
